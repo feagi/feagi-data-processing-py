d58a78ad814946fddc9fdb309e6cf3f10a8fd0ea
Amir Rasteg <amir.rasteg@protonmail.com>
2025-05-13 12:46:05 -0400

    reorganized file structure as per lib crate 0.0.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-mod feagi_data_vision;
 mod feagi_data_brain;
 mod feagi_byte_structures;
+mod brain_input;
+mod brain_output;
+mod byte_data_functions;
+mod cortical_area_state;
+mod neuron_state;
 
 use numpy::ndarray::AssignElem;
-use pyo3::{prelude::*};
+use pyo3::prelude::*;
 use pyo3::{wrap_pyfunction, wrap_pymodule};
 use pyo3::types::IntoPyDict;
 
-use feagi_data_vision::{register_data_vision};
+use brain_input::vision::register_data_vision;
 
 /// Core Module, accessible to users
 #[pymodule]
--- test.py
+++ test.py
@@ -7,3 +7,4 @@ print(a.lower_right())
 
 b = fdp.data_vision.peripheral_segmentation.SegmentedVisionCenterProperties((0.2, 0.3), (0.4, 0.6))
 print(b.calculate_pixel_coordinates_of_center_corners((1000, 1000)).lower_right())
+print("pause")
