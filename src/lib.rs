549ed647fa5cea91962af9849480c765d9825d50
Amir Rasteg <amir.rasteg@protonmail.com>
2025-06-02 11:37:43 -0400

    add func function

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,22 @@ use pyo3::{wrap_pyfunction, wrap_pymodule};
 use pyo3::types::IntoPyDict;
 
 
+fn check_submodule_exists(parent: &Bound<'_, PyModule>, submodule_name: &str) -> bool {
+    match parent.getattr(submodule_name) {
+        Ok(attr) => attr.is_instance_of::<PyModule>(),
+        Err(_) => false,
+    }
+}
 
 macro_rules! add_python_class {
     ($python:expr, $root_python_module:expr, $class_path:expr, $class:ty) => {
         {
-            fn check_submodule_exists(py: Python, parent: &Bound<'_, PyModule>, submodule_name: &str) -> bool {
-                match parent.getattr(submodule_name) {
-                    Ok(attr) => attr.is_instance_of::<PyModule>(),
-                    Err(_) => false,
-                }
-            }
             
             let path: Vec<String> = $class_path.split('.').map(|s| s.to_string()).collect();
             let mut current_module = $root_python_module.clone();
             
             for path_step in path {
-                if !check_submodule_exists($python, &current_module, &path_step) {
+                if !check_submodule_exists(&current_module, &path_step) {
                     // we need to add a submodule
                     let child_module = PyModule::new_bound($python, &path_step)?;
                     current_module.add_submodule(&child_module)?;
@@ -42,6 +42,34 @@ macro_rules! add_python_class {
         }
     };
 }
+
+macro_rules! add_python_function {
+    ($python:expr, $root_python_module:expr, $class_path:expr, $function:ty) => {
+        {
+            
+            let path: Vec<String> = $class_path.split('.').map(|s| s.to_string()).collect();
+            let mut current_module = $root_python_module.clone();
+            
+            for path_step in path {
+                if !check_submodule_exists(&current_module, &path_step) {
+                    // we need to add a submodule
+                    let child_module = PyModule::new_bound($python, &path_step)?;
+                    current_module.add_submodule(&child_module)?;
+                    current_module = child_module;
+                }
+                else {
+                    // child module already exists. Switch to it
+                    let child_module = current_module.getattr(&path_step)?;
+                    current_module = child_module.downcast::<PyModule>()?.clone();
+                }
+            }
+            
+            current_module.add_function::<$function>()?;
+        }
+    };
+}
+
+// TODO the above macros can be consolidated
 
 
 /// Core Module, accessible to users
@@ -70,9 +98,4 @@ fn feagi_data_processing(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     add_python_class!(py, m, "brain_input.vision", brain_input::vision::quick_image_diff::PyQuickImageDiff);
     
     Ok(())
-}
-
-#[pyfunction]
-fn test_function() -> String {
-    "Subfunction".to_string()
 }
