15af2190e4e56a15bab883f06e6d8c66749798dd
Amir Rasteg <amir.rasteg@protonmail.com>
2025-06-04 17:00:07 -0400

    updated test, version

--- test.py
+++ test.py
@@ -7,29 +7,38 @@ print("start")
 
 
 
+# cortical areas and their neurons
+cortical_id_a = fdp.cortical_data.CorticalID("AAAAAA")
+neuron_a_1 = fdp.neuron_data.neurons.NeuronXYZP(1,2,3,0.5)
+neuron_a_2 = fdp.neuron_data.neurons.NeuronXYZP(4,5,6,0.2)
+neurons_a = fdp.neuron_data.neuron_arrays.NeuronXYZPArrays(2)
+neurons_a.add_neuron(neuron_a_1)
+neurons_a.add_neuron(neuron_a_2)
 
-cortical_id = fdp.cortical_data.CorticalID("abcdef")
-
-neuron_1 = fdp.neuron_data.neurons.NeuronXYZP(1,2,3,0.5)
-neuron_2 = fdp.neuron_data.neurons.NeuronXYZP(4,5,6,0.1)
-
-
-
-created_neuron_arr = fdp.neuron_data.neuron_arrays.NeuronXYZPArrays(2)
-created_neuron_arr.add_neuron(neuron_1)
-created_neuron_arr.add_neuron(neuron_2)
+cortical_id_b = fdp.cortical_data.CorticalID("BBBBBB")
+neuron_b_1 = fdp.neuron_data.neurons.NeuronXYZP(8,9,10,0.5)
+neuron_b_2 = fdp.neuron_data.neurons.NeuronXYZP(11,12,13,0.2)
+neurons_b = fdp.neuron_data.neuron_arrays.NeuronXYZPArrays(2)
+neurons_b.add_neuron(neuron_b_1)
+neurons_b.add_neuron(neuron_b_2)
 
 
-print(created_neuron_arr.get_number_of_neurons_used())
-list_of_neurons = created_neuron_arr.copy_as_neuron_xyzp_vec()
+# list_of_neurons = neurons_a.copy_as_neuron_xyzp_vec() # example, getting as vector
 
-mapped_neuron_data = fdp.neuron_data.neuron_mappings.CorticalMappedXYZPNeuronData()
-mapped_neuron_data.insert(cortical_id, created_neuron_arr)
+generated_mapped_neuron_data = fdp.neuron_data.neuron_mappings.CorticalMappedXYZPNeuronData()
+generated_mapped_neuron_data.insert(cortical_id_a, neurons_a)
+generated_mapped_neuron_data.insert(cortical_id_b, neurons_b)
 
-for (c_id, neurons) in mapped_neuron_data.iter_easy():
-    print("a")
+for (c_id, neurons) in generated_mapped_neuron_data.iter_easy():
+    print("breakpoint")
 
-byte_data = mapped_neuron_data.as_new_feagi_byte_structure()
+byte_data = generated_mapped_neuron_data.as_new_feagi_byte_structure()
+bytes = byte_data.copy_as_bytes()
+
+received_byte_data = fdp.byte_structures.FeagiByteStructure(bytes)
+received_cortical_mappings = fdp.neuron_data.neuron_mappings.CorticalMappedXYZPNeuronData.from_feagi_byte_structure(received_byte_data)
+for (c_id, neurons) in received_cortical_mappings.iter_easy():
+    print("breakpoint")
 
 print("pause")
 #fake_image_source = np.zeros((2000,2000,3), dtype=np.float32)
